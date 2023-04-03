fd22170bbd420ce85a043cd115acd9b1c551eec3
reeNUT <rmurzaliev@yandex.ru>
2023-04-04 02:10:17 +0400

    homework3

--- task1.java
+++ task1.java
@@ -1,13 +1,80 @@
-public class task1 {
-    public static void main(String[] args) {
-        String s = "cba";
-        int[] index = {3, 2, 1};
-        StringBuilder z = new StringBuilder(s);
+import java.util.Random;
 
-        for (int i = 0; i < s.length(); i++) {
-            z.setCharAt(i, s.charAt(index[i]-1));
+public class task1 {
+    static int[] mergeSort(int[] m) {
+        int[] left;
+        int[] right;
+        int[] result;
+        if (m.length <= 1) return m;
+
+        int middle = m.length / 2;
+
+        left = new int[middle];
+        for (int i = 0; i < middle; i++) {
+            left[i] = m[i];
         }
 
-        System.out.println(z);
+        right = new int[middle];
+        for (int i = middle; i < m.length; i++) {
+            right[i-middle] = m[i];
+        }
+
+        left = mergeSort(left);
+        right = mergeSort(right);
+        result = new int[m.length];
+
+        int i = 0;
+        int j = 0;
+        for (int k = 0; k < result.length; k++) {
+            if (j >= right.length || (i < left.length && left[i] > right[j])) {
+                result[k] = left[i];
+                i++;
+            } else {
+                result[k] = right[j];
+                j++;
+            }
+        }
+        return result;
+    }
+
+    static int[] selectSort(int[] m) {
+        for (int i = 0; i < m.length; i++) {
+            int maxIdx = i;
+            for (int j = i; j < m.length; j++) {
+                if (m[j] > m[maxIdx]) maxIdx = j;
+            }
+            int x = m[i];
+            m[i] = m[maxIdx];
+            m[maxIdx] = x;
+        }
+        return m;
+    }
+
+    static void printArray(int[] m) {
+        for (int i = 0; i < m.length; i++) {
+            System.out.printf("%d ", m[i]);
+        }
+        System.out.println("\n");
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[8];
+        Random rnd = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rnd.nextInt(1, 100);
+        }
+
+        System.out.println("origin array:");
+        printArray(arr);
+        
+        int[] mergeSorted = mergeSort(arr);
+
+        System.out.println("merge sorted:");
+        printArray(mergeSorted);
+
+        int[] selectSorted = selectSort(arr);
+
+        System.out.println("select sorted:");
+        printArray(selectSorted);
     }
 }
